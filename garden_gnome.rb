81266a3621e23178ac0f53975600d5f3cd2b4aee
nimishat <nimishathakkar83@gmail.com>
2018-07-13 14:59:40 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,6 +1,6 @@
 class GardenGnome
-  attr_reader :name :personality
-  attr_writer :age :gluten_allergy
+  attr_reader :name, :personality
+  attr_writer :age, :gluten_allergy
   
   def initialize(hat_color)
     @personality = "evil"
