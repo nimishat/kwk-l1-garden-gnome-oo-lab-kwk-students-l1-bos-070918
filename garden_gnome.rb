ab1703df93d2744b0ebe45ee8cc2b0330853edfa
nimishat <nimishathakkar83@gmail.com>
2018-07-13 15:16:27 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -8,14 +8,14 @@ class GardenGnome
   end
   
   def gnaw
-    puts "Gnawing on a tree!!!"
+     "Gnawing on a tree!!!"
   end
   
   def shout
-    puts "GNARLY!!!"
+     "GNARLY!!!"
   end
   
     def introduce_self
-      puts "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
+       "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
     end
 end
